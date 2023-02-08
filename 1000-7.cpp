16c05d72acb5128be49218b058235c7fe7919c75
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 18:22:42 +0300

    1000-7
    Идея была взята из одного мультипликационного проекта.

---
+++ 1000-7.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+int main()
+{
+	system("color 0C");
+	int i = 1000;
+	while (i > 8)
+	{
+		i = i - 7;
+		std::cout << i << " - 7\n";
+
+		Sleep(5);
+	}
+	std::cout << "Dead Inside\n";
+	cout << "Ya gul";
+	return 0;
+}
