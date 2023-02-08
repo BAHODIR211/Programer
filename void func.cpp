810fe7152a692f876d54db28599513befcbbd0d1
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 19:38:47 +0300

    void func
    Тут рассмотрено один из главных видов использование  void func.
    Говоря простыми словами void его указывают в качестве типа возвращаемого значения функции, если она ничего не возвращает.

---
+++ void func.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+void func1();
+void func2();
+
+int main()
+{
+    system("color 0C");
+    int x;
+    x = 3;
+    func1();
+    func2();
+    cout << x; 
+    cout << "\n";
+    cout << "This is finish";
+    return 0;
+}
+void func1()
+ {
+int x;
+x = 1;
+cout << x;
+cout << "\n";
+ }
+void func2()
+{
+    int x;
+    x = 2;
+    cout << x;
+    cout << "\n";
+}
