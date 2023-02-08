c54d475d88fa5ccec41656cdba8518e905a9d702
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 20:22:28 +0300

    Этот код предназначен для справкию
    Этот код предназначен для справки а простыми словами информации об трёх элементах и кончноже вы можете выбрать идин из 3.

---
+++ Kod dlya spravki.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    system("color 0E");
+    int choice;
+    cout << "Spravkka po temam:\n\n";
+    cout << "1. for\n";
+    cout << "2. if\n";
+    cout << "3. switch\n\n";
+
+    cout << "vvedite nomer temi(1-3):";
+    cin >> choice;
+    cout << "\n";
+    switch (choice) {
+    case 1:
+        cout << "for - eto samiy unuvursalyniy cikl v C++.\n";
+        break;
+    case 2:
+        cout << "if - eto instrukci uslovnogo vostavleniye.\n";
+        break;
+    case 3:
+        cout << "switch - eto instrukcya mnogopravlennogo votstavleniya.\n";
+        break;
+    default:
+        cout << "vi doljni vvesti chislo ot 1 do 3.\n";
+        return 0;
+    }
+}
