7ebf91963fe952ba3f52b808cbc2e727fc8a6d6f
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 19:52:57 +0300

    Код для получения простых математических примеров.
    В этом коде используется такие функции как например rand( ) он отвечает за рандомные выбор числа или можно сказать за число которое выбрано случайным образом.

---
+++ skolkota primerov.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void drill();
+int bob;
+int num_right;
+int main()
+{
+
+   cout << "Nechta masalla: \n";
+   cin >> bob;
+   num_right = 0;
+   do {
+	drill(); bob--;
+   } while (bob);
+   cout << "Siz berdingiz:"<< num_right << " tog'ri javoblar.\n";
+   return 0;
+}
+
+void drill()
+
+{
+	int bob;
+	int a, b, ans;
+	a = rand() % 100;
+	b = rand() % 100;
+	for (bob = 0; bob < 3; bob++) {
+		cout << "Nechi boladi" << a << " + " << b << "? ";
+		cin >> ans;
+		if (ans == a + b) {
+			cout << "To'ri javob\n";
+			num_right++;
+			return;
+		}
+	}
+	cout << "Siz hamma javoblarni ishlatingiz.\n";
+	cout << "JAvobi barobar " << a + b << '\n';
+}
