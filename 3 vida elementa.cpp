ea04921ed08e307dccbb6f10f43bf923aabb49be
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 18:50:54 +0300

    Тут присутствуют 3 вида элементов
    В этом коде присутствуют элементы 3 видов элемента а точнее говоря: int short, long, и double.

---
+++ 3 vida elementa.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system("color 0C");
+    int b;
+    long h;
+    double c;
+    b = atoi("10");
+    h = atol("1000000");
+    c = atof("-0.123");
+    cout << b << ' ' << h << ' ' << c;
+    cout << ' \n';
+
+    return 0;
+}
