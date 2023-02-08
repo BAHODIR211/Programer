d237e57eb8264696882685f17ec2d7844bd14167
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 20:14:05 +0300

    Этот код про продолжительность.
    этот код про continiun продолжительность он будет ити до определёной цифры по полном и неполным виде и это не ошибка кода.

---
+++ continiun.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    system("color 0D");
+    int x;
+    for (x = 0; x <= 100000; x++) {
+        if (x % 2) continue;
+        cout << x << ' ';
+    }
+    return 0;
+}
