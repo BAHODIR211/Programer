ec8316be4b86d024fe6917b77d10ecee7346419c
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 20:17:15 +0300

    Числа в квадрате.
    Этот код делает квадратный премеры а точнее просто квадратный упражнения.

---
+++ V kvadrate ravno.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	system("color 0A");
+	for (int i = 0; i < 10; i++) {
+		cout << i << " ";
+		cout << "V kvadrete ravno" << i * i << "\n";
+	}
+
+}
