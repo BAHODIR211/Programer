60ee82054c058e15b1365294b343b558c2aaf153
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-08 20:32:35 +0300

    Kalkulyator
    Kalkulyator сначала вводите 2 числа а после введите элемент на который вы можете кпримеру делить или умножать или + , - .

---
+++ Kalkulator.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    float   ch1, ch2, res;
+    char    sim;
+    cout << "Soon: \n";
+    cin >> ch1 >> ch2;
+    cout << "Znak:\n";
+    cin >> sim;
+
+    switch(sim)
+
+    {
+    case '*':
+        res = ch1 * ch2;
+        break;
+    case '/':
+        res = ch1 / ch2;
+        break;
+    case '-':
+        res = ch1 - ch2;
+        break;
+    case '+':
+        res = ch1 + ch2;
+        break;
+    }
+        cout<<ch1<<sim<<ch2<<"="<<res<<endl;
+        system("pause");
+        return 0;
+    }
