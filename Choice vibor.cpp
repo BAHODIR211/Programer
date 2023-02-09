68124fca0c38c85e70870643e4ac86c2ef1385be
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-09 22:02:42 +0300

    choice.
    Choice переводится ка выбор и делает он такуюже вункцию в коде.

---
+++ Choice vibor.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	system("color 0C");
+	int choice;
+	cout << "Vvedide chislo:";
+	cin >> choice;
+	if (choice == 1) {
+		int a, b;
+		cout << "2 Son kiriting ";
+		cin >> a >> b;
+		cout << "Javobi barobar" << a + b << '\n';
+	}
+	else {
+		char s1[80], s2[80], s3[80];
+		cout << "3 Stroka yozing: ";
+		cin >> s1;
+		cin >> s2;
+		cin >> s3;
+		cout << ":...FINISH...:";
+	}
+}
