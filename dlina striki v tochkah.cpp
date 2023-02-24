6ee7634b0eba8530b7433b037cff8e6d90583a31
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-24 21:10:25 +0300

    очень прикольный код
    его можно использовать для спама и т.д.

---
+++ dlina striki v tochkah.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("COLOR 0A");
+	int B;
+	cout << "Vvedite dllinu stroki ot 1 do... : ";
+	cin >> B;
+	while (B > 0 && B < 7000000) {
+		cout << '.';
+		B--;
+	}
+	return 0;
+}
