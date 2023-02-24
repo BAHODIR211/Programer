89f9a090e2411500aeca20e4cd66d64f19887c09
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-24 21:12:09 +0300

    Этот код для сейфа.

---
+++ kod dlya seyfa.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("color 0C");
+	int num;
+	do {
+		cout << " vvedi parol:::";
+		cin >> num;
+	} while (num != 123);
+	if (num) cout << "PAROL TOG'RI SEYF OCHILDI";
+	return 0;
+}
