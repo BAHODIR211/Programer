8a2794e52d9e3feb394fe29dcf191bbfcf51b29d
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-02-27 21:35:35 +0300

    Считает до 1000 .
    этот код показывает цифры от 0 до 1000 в порядке возрастания и пишеться через enterю

---
+++ saniydi pasga 1000 ga cha.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("color 0D");
+    int i, j;
+    for (i = 2; i < 1000; i++) {
+        for (j = 2; j <= (i / j); j++)
+            if (!(i % j)) break;
+        if (j > (i / j)) cout << i << " - Odiy raqam\n";
+    }
+    return 0;
+}
