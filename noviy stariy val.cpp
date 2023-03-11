08410299c579e31da3df5e68ab66b741907df904
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-03-11 15:12:17 +0300

    val

---
+++ noviy stariy val.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+void f(int& i);
+int main()
+{
+	system("color 0A");
+		int val = 1;
+		cout << "Staroye znacheniye val: " << val << '\n';
+		f(val);
+		cout << "Novoy znachaniye val: " << val << '\n';
+		return 0;
+	
+}
+	void f(int& i)
+{
+	i = 10;
+}
