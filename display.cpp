5c3d80f56100b339335d4eba1c96e00d0673bc17
BAHODIR211 <124704712+BAHODIR211@users.noreply.github.com>
2023-03-11 15:11:48 +0300

    код для команды display

---
+++ display.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+void display(int num);
+int main()
+{
+	system("color 0C");
+	int t[10], i;
+	for (i = 0; i < 10; ++i) t[i] = i;
+	for (i = 0; i < 10; i++) display(t[10]);
+	return 0;
+}
+void display(int num)
+{
+	cout << num << '\n';
+}
